975d62f49c90d040d4c647ade970c85790159752
phoenixhell <phoenixrever@gmail.com>
2023-11-04 22:57:47 +0900

    DispatchServletDemo

---
+++ src/main/java/com/phoenixhell/zerokaraspring/mvc/servlet/WebConfig.java
@@ -0,0 +1,84 @@
+package com.phoenixhell.zerokaraspring.mvc.servlet;
+
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletRegistrationBean;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Configuration
+@ComponentScan
+@PropertySource("classpath:application.properties")
+@EnableConfigurationProperties({WebMvcProperties.class, ServerProperties.class})
+public class WebConfig {
+    //配置内嵌web容器的配置类 以下3个是必须配置的
+    // ⬅️内嵌 web 容器工厂
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory(ServerProperties serverProperties) {
+        return new TomcatServletWebServerFactory(serverProperties.getPort());
+    }
+
+    // ⬅️创建 DispatcherServlet  处理http请求  在首次使用到时候 由tomcat 来初始化
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    // ⬅️注册 DispatcherServlet 到tomcat容器中  DispatcherServlet需要tomcat 环境才能运行, Spring MVC 的入口
+    @Bean
+    public DispatcherServletRegistrationBean dispatcherServletRegistrationBean(DispatcherServlet dispatcherServlet, WebMvcProperties webMvcProperties){
+        // DispatcherServletRegistrationBean registrationBean = new DispatcherServletRegistrationBean(dispatcherServlet, "/");
+        // 大于0的值会在tomcat启动时候进行初始化 默认-1
+        //数字是因为由多个dispatchServlet的时候数字小的优先级更高
+        //registrationBean.setLoadOnStartup(1);
+        //return registrationBean;
+        DispatcherServletRegistrationBean registrationBean = new DispatcherServletRegistrationBean(dispatcherServlet, "/");
+        registrationBean.setLoadOnStartup(webMvcProperties.getServlet().getLoadOnStartup());
+        return registrationBean;
+    }
+
+    //// 如果用 DispatcherServlet 初始化时默认添加的组件, 并不会作为 bean, 给测试带来困扰
+    //// ⬅️1. 加入RequestMappingHandlerMapping
+    //@Bean
+    //public RequestMappingHandlerMapping requestMappingHandlerMapping() {
+    //    return new RequestMappingHandlerMapping();
+    //}
+    //
+    //// ⬅️2. 继续加入RequestMappingHandlerAdapter, 会替换掉 DispatcherServlet 默认的 4 个 HandlerAdapter
+    //@Bean
+    //public MyRequestMappingHandlerAdapter requestMappingHandlerAdapter() {
+    //    TokenArgumentResolver tokenArgumentResolver = new TokenArgumentResolver();
+    //    YmlReturnValueHandler ymlReturnValueHandler = new YmlReturnValueHandler();
+    //    MyRequestMappingHandlerAdapter handlerAdapter = new MyRequestMappingHandlerAdapter();
+    //    handlerAdapter.setCustomArgumentResolvers(List.of(tokenArgumentResolver));
+    //    handlerAdapter.setCustomReturnValueHandlers(List.of(ymlReturnValueHandler));
+    //    return handlerAdapter;
+    //}
+    //
+    //public HttpMessageConverters httpMessageConverters() {
+    //    return new HttpMessageConverters();
+    //}
+
+    // ⬅️3. 演示 RequestMappingHandlerAdapter 初始化后, 有哪些参数、返回值处理器
+
+    // ⬅️3.1 创建自定义参数处理器
+
+    // ⬅️3.2 创建自定义返回值处理器
+
+}
